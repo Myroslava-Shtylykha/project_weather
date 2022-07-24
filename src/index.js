c7ccf761bbf2eb5cf6d159a547ebb58c5ca457d3
Myroslava-Shtylykha <mirunja@gmail.com>
2022-07-25 00:43:00 +0300

    Improved the project according to the HW week 7

--- src/index.js
+++ src/index.js
@@ -1,45 +1,101 @@
-//1
-let now = new Date();
-let date = now.getDay();
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let day = days[now.getDay()];
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+/// Time
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let currently = document.querySelector(".day_time");
-currently.innerHTML = `${day} ${hours}:${minutes}`;
 
-
-//2
+///  Weather, icon
 function temperature(response) {
-let temperature = Math.round(response.data.main.temp);
-let temperatureCity = document.querySelector(".degrees_now");
-temperatureCity.innerHTML = `${temperature}`;
-let cityGeo = document.querySelector("#city_now");
-cityGeo.innerHTML = response.data.name;
+  console.log(response.data);
+  let city = document.querySelector("#city_now");
+  let temperatureCity = document.querySelector(".degrees_now");
+  let descriptionWeather = document.querySelector(".description");
+  let wind = document.querySelector("#wind_card");
+  let humidity = document.querySelector("#humidity_card");
+  let pressure = document.querySelector("#pressure_card");
+  let icon = document.querySelector("#icon");
+  let dateElement = document.querySelector(".day_time");
+
+  celsiusTemperature = Math.round(response.data.main.temp);
+
+  city.innerHTML = response.data.name;
+  pressure.innerHTML = response.data.main.pressure;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  descriptionWeather.innerHTML = response.data.weather[0].main;
+  temperatureCity.innerHTML = Math.round(response.data.main.temp);
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
+function search(city) {
+  let apiKey = "79ecd77b9336a83dd439bcbfd2adb260";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(`${apiUrl}`).then(temperature);
+}
 
 function currentSity(event) {
-event.preventDefault();
-let textInput = document.querySelector("#city");
-let changeCity = document.querySelector("#city_now");
-changeCity.innerHTML = `${textInput.value}`;
- let apiKey = "79ecd77b9336a83dd439bcbfd2adb260";
- let units = "metric";
- let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${textInput.value}&appid=${apiKey}&units=${units}`;
-
-axios.get(`${apiUrl}`).then(temperature);
+  event.preventDefault();
+  let citySearchElement = document.querySelector("#city");
+  search(citySearchElement.value);
 }
+
+/// Fahr/Cels
+
+function changeFahrenheit(event) {
+  event.preventDefault();
+  let degreesNow = document.querySelector(".degrees_now");
+  celsius.classList.remove("active");
+  fahrenheit.classList.add("active");
+  let fahrenheitTemperature = celsiusTemperature * 1.8 + 32;
+  degreesNow.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function changeCelsius(event) {
+  event.preventDefault();
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
+  let degreesNow = document.querySelector(".degrees_now");
+  degreesNow.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let city = document.querySelector("#city_search");
 city.addEventListener("submit", currentSity);
 
+let fahrenheit = document.querySelector("#icon_fahrenheit");
+fahrenheit.addEventListener("click", changeFahrenheit);
+
+let celsius = document.querySelector("#icon_celsius");
+celsius.addEventListener("click", changeCelsius);
+
+search("Sydney");
+
+/*Поточна геопозиція
 
 function showPosition(position) {
   let apiKey = "79ecd77b9336a83dd439bcbfd2adb260";
@@ -58,47 +114,4 @@ function getTemp(event) {
 }
 
 let button = document.querySelector(".searchGeo");
-button.addEventListener("click", getTemp);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//3
-/*function changeCelsius (event) {
-  event.preventDefault();
-  degreesNow.innerHTML = `25`;
-}
-let celsFahr = document.querySelector("#icon_celsius");
-let degreesNow = document.querySelector(".degrees_now");
-celsFahr.addEventListener("click", changeCelsius);
-
-
-function changeFahrenheit(event) {
-  event.preventDefault();
-  degreesNow.innerHTML = ((25 * 1.8) + 32);
-}
-let fahrenheit = document.querySelector("#icon_fahrenheit");
-fahrenheit.addEventListener("click", changeFahrenheit);*/
-
-//HW W5
-
-
-/*function temperature(response) {
-  let apiKey = "79ecd77b9336a83dd439bcbfd2adb260";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  console.log(response.data);
-
-
-axios.get(apiUrl).then(temperature);
-}*/
+button.addEventListener("click", getTemp);*/
